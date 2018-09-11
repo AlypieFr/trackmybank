bddf511d82961e3e8e821a03c70f1e7417ede176
Floréal Cabanettes <floreal.cab@flo-art.fr>
2018-09-11 20:22:15 +0200

    Make skeleton working: views working

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def data(self):
+        dat = {}
+        dat["mavar"] = "Ouais salut connard !"
+
+        return dat
