b9e052044a62035911f33803fcb053671460c54a
Floréal Cabanettes <floreal.cab@flo-art.fr>
2018-09-22 16:03:37 +0200

    Add copyright headers

--- main/apps.py
+++ main/apps.py
@@ -1,3 +1,11 @@
+# TrackMyBank, a simple bank management tool
+# 
+# Copyright (C) 2018 Floreal Cabanettes <comm@flo-art.fr>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3.0
+
 from django.apps import AppConfig
 
 
--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+
+# TrackMyBank, a simple bank management tool
+# 
+# Copyright (C) 2018 Floreal Cabanettes <comm@flo-art.fr>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3.0
+
 import os
 import sys
 
