c7f818afa7360728f5f98c15fcfc61d119d90589
Floréal Cabanettes <floreal.cab@flo-art.fr>
2019-07-28 12:00:17 +0200

    Migrate to django 2.2

--- main/models.py
+++ main/models.py
@@ -27,8 +27,8 @@ class UserRole(models.Model):
         unique_together = (("user", "role"),)
         verbose_name = _("User role")
 
-    user = models.ForeignKey(User, verbose_name=_("User"))
-    role = models.ForeignKey(Role, verbose_name=_("Role"))
+    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, verbose_name=_("Role"), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username + " - " + _(self.role.name)
@@ -51,8 +51,8 @@ class UserGroup(models.Model):
         verbose_name = _("User group")
         verbose_name_plural = _("User groups")
 
-    user = models.OneToOneField(User, primary_key=True, verbose_name=_("User"))
-    group = models.ForeignKey(Group, verbose_name=_("Group"))
+    user = models.OneToOneField(User, primary_key=True, verbose_name=_("User"), on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, verbose_name=_("Group"), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username + " - " + _(self.group.name)
@@ -77,7 +77,7 @@ class Month(models.Model):
     month = models.IntegerField(verbose_name=_("month"))
     year = models.IntegerField(verbose_name=_("year"))
     salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("salary"))
-    ugroup = models.ForeignKey(Group, verbose_name=_("user group"))
+    ugroup = models.ForeignKey(Group, verbose_name=_("user group"), on_delete=models.CASCADE)
 
     def __str__(self):
         with calendar.different_locale(settings.LOCALE):
@@ -90,14 +90,14 @@ class Month(models.Model):
 
 
 class CurrentMonth(models.Model):
-    group = models.OneToOneField(Group, primary_key=True)
-    month = models.ForeignKey(Month, verbose_name=_("month"))
+    group = models.OneToOneField(Group, primary_key=True, on_delete=models.CASCADE)
+    month = models.ForeignKey(Month, verbose_name=_("month"), on_delete=models.CASCADE)
 
 
 class TransactionGroup(models.Model):
     date_t = models.DateField(verbose_name=_("transaction date"))
     date_bank = models.DateField(verbose_name=_("bank added date"), blank=True, null=True)
-    month = models.ForeignKey(Month, verbose_name=_("month"))
+    month = models.ForeignKey(Month, verbose_name=_("month"), on_delete=models.CASCADE)
     ignore_week_filters = models.BooleanField(default=False, verbose_name=_("ignore weekly spending filters"))
 
     def __str__(self):
@@ -112,8 +112,8 @@ class TransactionGroup(models.Model):
 class Transaction(models.Model):
     subject = models.CharField(max_length=255, verbose_name=_("subject"))
     amount = models.DecimalField(max_digits=7, decimal_places=2, verbose_name=_("amount"))
-    category = models.ForeignKey(Category, verbose_name=_("category"))
-    group = models.ForeignKey(TransactionGroup, verbose_name=_("group"))
+    category = models.ForeignKey(Category, verbose_name=_("category"), on_delete=models.CASCADE)
+    group = models.ForeignKey(TransactionGroup, verbose_name=_("group"), on_delete=models.CASCADE)
 
     def __str__(self):
         return " ".join((calendar.month_name[self.group.month.month] + "_" + str(self.group.month.year), "-",
@@ -125,7 +125,7 @@ class Transaction(models.Model):
 
 
 class RecurringCharges(models.Model):
-    group = models.OneToOneField(Group, primary_key=True)
+    group = models.OneToOneField(Group, primary_key=True, on_delete=models.CASCADE)
     file = models.CharField(max_length=1000, verbose_name=_("File path"))
 
     def __str__(self):
